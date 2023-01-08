6b0dde8bcbcec94b13c30a32db03eced3303a2ac
Julian <cusco2891@gmail.com>
2023-01-08 02:08:48 -0500

    3item row needs bkgrnd-img high opacity // videorow/img needs rounded border+vid + dynamic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5131130cbc19c12c"; }
+/******/ 		__webpack_require__.h = function() { return "eb990886de9d1e3c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.8beb3759ba879ae5.hot-update.js
+++ .next/static/webpack/webpack.8beb3759ba879ae5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f8956a13b3c6d48c"; }
+/******/ 	__webpack_require__.h = function() { return "9f32159bd653f780"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
